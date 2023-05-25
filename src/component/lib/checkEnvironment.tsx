f70918c0b15dc96a61a8fbb6cd31024f5b01b262
Daniel Yu <charlesyou1234567@gmail.com>
2023-05-26 00:34:16 +0800

    test sign

--- src/component/lib/checkEnvironment.tsx
+++ src/component/lib/checkEnvironment.tsx
@@ -1,7 +1,7 @@
 export function checkEnvironment(apiRoute="string"){
     let api: string;
     if (process.env.NODE_ENV !== 'development'&&typeof process.env.NEXT_PUBLIC_ONLINESERVER == "string") {
-        api=process.env.NEXT_PUBLIC_ONLINESERVER;
+        api=process.env.NEXT_PUBLIC_ONLINESERVER+apiRoute;
         return{api}
       }else{
         api='http://localhost:3030'+apiRoute;
